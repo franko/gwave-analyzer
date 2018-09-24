8328277b77fb5cf83065d82b4239c932c2fd749c
Francesco ABBATE <francesco abbate@st.com>
2018-09-24 14:33:47 +0200

    Return error codes on wav header errors
    Previous method was writing on a string and it was unclear.

--- wav_reader.h
+++ wav_reader.h
@@ -15,9 +15,16 @@ typedef struct {
     int canal_method;
 } wav_reader_t;
 
+enum {
+    WAV_READER_SUCCESS = 0,
+    WAV_READER_NO_FILE,
+    WAV_READER_UNKNOWN_FORMAT,
+    WAV_READER_ILL_FORMED,
+};
+
 extern long wav_reader_offset(wav_reader_t *wav, int pos);
 extern int wav_reader_seek(wav_reader_t *wav, int pos);
-extern void wav_reader_init(wav_reader_t *wav, const char *filename, char status_message[], const int status_message_len);
+extern int wav_reader_init(wav_reader_t *wav, const char *filename);
 extern void wav_reader_set_zero(wav_reader_t *wav);
 
 extern int getone(wav_reader_t *wav, double x[]);
