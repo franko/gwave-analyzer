99ff356a2d831f9acab6d82df10baeda98162921
Francesco Abbate <francesco.bbt@gmail.com>
2018-11-05 17:52:44 +0100

    WIP: working on libui wav display

--- ui-main.c
+++ ui-main.c
@@ -2,6 +2,11 @@
 #include <string.h>
 #include <ui.h>
 
+// names and values from https://msdn.microsoft.com/en-us/library/windows/desktop/dd370907%28v=vs.85%29.aspx
+#define colorWhite 0xFFFFFF
+#define colorBlack 0x000000
+#define colorDodgerBlue 0x1E90FF
+
 static int onClosing(uiWindow *w, void *data) {
     uiQuit();
     return 1;
@@ -13,13 +18,74 @@ static int onShouldQuit(void *data) {
     return 1;
 }
 
+// helper to quickly set a brush color
+static void setSolidBrush(uiDrawBrush *brush, uint32_t color, double alpha)
+{
+    uint8_t component;
+
+    brush->Type = uiDrawBrushTypeSolid;
+    component = (uint8_t) ((color >> 16) & 0xFF);
+    brush->R = ((double) component) / 255;
+    component = (uint8_t) ((color >> 8) & 0xFF);
+    brush->G = ((double) component) / 255;
+    component = (uint8_t) (color & 0xFF);
+    brush->B = ((double) component) / 255;
+    brush->A = alpha;
+}
+
+static void handlerDraw(uiAreaHandler *a, uiArea *area, uiAreaDrawParams *p) {
+    uiDrawBrush brush;
+    setSolidBrush(&brush, colorWhite, 1.0);
+
+    uiDrawPath *path = uiDrawNewPath(uiDrawFillModeWinding);
+    uiDrawPathAddRectangle(path, 0, 0, p->AreaWidth, p->AreaHeight);
+    uiDrawPathEnd(path);
+    uiDrawFill(p->Context, path, &brush);
+    uiDrawFreePath(path);
+}
+
+static void handlerMouseEvent(uiAreaHandler *a, uiArea *area, uiAreaMouseEvent *e) {
+#if 0
+    double graphWidth, graphHeight;
+    double xs[10], ys[10];
+    int i;
+
+    graphSize(e->AreaWidth, e->AreaHeight, &graphWidth, &graphHeight);
+    pointLocations(graphWidth, graphHeight, xs, ys);
+
+    for (i = 0; i < 10; i++)
+        if (inPoint(e->X, e->Y, xs[i], ys[i]))
+            break;
+    if (i == 10)        // not in a point
+        i = -1;
+
+    currentPoint = i;
+    // TODO only redraw the relevant area
+    uiAreaQueueRedrawAll(histogram);
+#endif
+}
+
+static void handlerMouseCrossed(uiAreaHandler *ah, uiArea *a, int left)
+{
+    // do nothing
+}
+
+static void handlerDragBroken(uiAreaHandler *ah, uiArea *a)
+{
+    // do nothing
+}
+
+static int handlerKeyEvent(uiAreaHandler *ah, uiArea *a, uiAreaKeyEvent *e)
+{
+    // reject all keys
+    return 0;
+}
+
 int main() {
     uiInitOptions options;
     memset(&options, 0, sizeof (uiInitOptions));
     const char *err = uiInit(&options);
-    /* workaround: in libui alpha4.1 Common Controls return an error but with code 0, so there
-       is actually no error. */
-    if (err != NULL && strstr(err, "initializing Common Controls; code 0 ") == NULL) {
+    if (err != NULL) {
         fprintf(stderr, "%s", err);
         uiFreeInitError(err);
         return 1;
@@ -34,11 +100,13 @@ int main() {
     uiMenuItem *aboutItem = uiMenuAppendAboutItem(helpMenu);
 
     uiWindow *mainwin = uiNewWindow("GWave analyzer", 640, 480, 1);
+    uiWindowSetMargined(mainwin, 1);
     uiWindowOnClosing(mainwin, onClosing, NULL);
     uiOnShouldQuit(onShouldQuit, mainwin);
 
     uiBox *vbox = uiNewVerticalBox();
     uiBoxSetPadded(vbox, 1);
+    uiWindowSetChild(mainwin, uiControl(vbox));
 
     uiBox *hbox = uiNewHorizontalBox();
     uiBoxSetPadded(hbox, 1);
@@ -63,8 +131,15 @@ int main() {
     // uiRadioButtonsOnSelected(rb, onRBSelected, NULL);
     uiBoxAppend(hbox, uiControl(rb), 0);
 
-    uiWindowSetChild(mainwin, uiControl(vbox));
-    uiWindowSetMargined(mainwin, 1);
+    uiAreaHandler wav_display_handler[1];
+    wav_display_handler->Draw = handlerDraw;
+    wav_display_handler->MouseEvent = handlerMouseEvent;
+    wav_display_handler->MouseCrossed = handlerMouseCrossed;
+    wav_display_handler->DragBroken = handlerDragBroken;
+    wav_display_handler->KeyEvent = handlerKeyEvent;
+
+    uiArea *wav_display_area = uiNewArea(wav_display_handler);
+    uiBoxAppend(vbox, uiControl(wav_display_area), 1);
 
     uiControlShow(uiControl(mainwin));
     uiMain();
